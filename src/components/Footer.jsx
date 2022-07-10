a43d676fe664a1a9894ba49596443abab2605c6b
bshekhar123 <bshekhar2001@gmail.com>
2022-07-10 23:37:28 +0530

    bh

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,15 +8,16 @@ const Footer = () => {
     const year = new Date().getFullYear();
     return (
         <>
-            <div className="w-full py-16   bg-gray-800 ">
+            <div className="w-full py-16 pb-2  bg-gray-800 ">
                 <div className="max-w-[1240px] mx-auto  flex flex-col px-14">
                     <div className="sm:flex   justify-between items-center">
-                        <img className="p-7 float-left" width="200px" src={logo} />
+                        <img className="p-7 m-auto" width="200px" src={logo} />
                         <div className="  items-center content-center  justify-between my-6 justify-content  ">
                             <h5 className=" py-5 text-4xl text-orange-500">We Are...</h5>
                             <p className=" text-sm text-white">We are non-profit organization that operates independently of any government, mainly we aim to educate underprivileged students and help them in their pursuit of their dream.</p>
                         </div>
-                        <div className="flex justify-between mr-10 my-6 justify-content w-full sm:max-w-[200px]   ">
+                        
+                        <div className="flex justify-between lg:ml-14 my-6 justify-content w-full   sm:max-w-[200px]   ">
                             <a href="https://www.facebook.com/" target="_blank" className="">   <FaFacebook className="icon text-2xl text-gray-400" /></a>
                             <a href="mailto:swamivivekanandatrustkkr@gmail.com" target="_blank" className="">   <AiFillMail className="icon text-gray-400  text-2xl " /></a>
 
